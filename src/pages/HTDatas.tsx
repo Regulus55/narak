4009708899e097b85e7c0ea4b85db21d3f2dd880
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2024-12-21 02:18:41 +0900

    hantoo: network error/ need to check url

--- src/pages/HTDatas.tsx
+++ src/pages/HTDatas.tsx
@@ -22,38 +22,45 @@ const HTDatas = () => {
   };
 
   const getStockData = async () => {
-    const url = "https://api.koreainvestment.com/v1/market/stock";
+    const url =
+      "https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/quotations/inquire-price";
     const headers = createHeaders();
 
     try {
+      //   const res = await axios.get(
+      //     "http://ec2-13-125-246-160.ap-northeast-2.compute.amazonaws.com:8080/companies/123456"
+      //   );
+      //   console.log("reserserserserser", res);
       const response = await axios.get(url, { headers });
       if (response.status === 200) {
         setStockData(response.data);
       } else {
         setError(`Error: ${response.status}`);
       }
-    } catch (error) {
-      setError("API 호출 중 오류 발생");
+    } catch (error: any) {
+      console.error("API 호출 중 오류 발생:", error);
+      setError(`API 호출 중 오류 발생: ${error.message || error}`);
     }
   };
 
-  useEffect(() => {
-    if (!apiKey || !apiSecret) {
-      setError("API Key 또는 API Secret이 설정되지 않았습니다.");
-      return;
-    }
-    getStockData();
-  }, [apiKey, apiSecret]);
+  //   useEffect(() => {
+  //     if (!apiKey || !apiSecret) {
+  //       setError("API Key 또는 API Secret이 설정되지 않았습니다.");
+  //       return;
+  //     }
+  //     getStockData();
+  //   }, [apiKey, apiSecret]);
 
   return (
     <div>
       <h1>HT Datas</h1>
-      {error && <p>{error}</p>}
+      {/* {error && <p>{error}</p>}
       {stockData ? (
         <pre>{JSON.stringify(stockData, null, 2)}</pre>
       ) : (
         <p>로딩중...</p>
-      )}
+      )} */}
+      <button onClick={getStockData}>버튼불러오가ㅣ기기기기</button>
     </div>
   );
 };
