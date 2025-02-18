3432585cb6ab47aa8fda3599055bf1f3c1eac970
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2025-02-19 02:43:54 +0900

    styled component added

--- src/components/Layout/SideNavbar.tsx
+++ src/components/Layout/SideNavbar.tsx
@@ -2,21 +2,35 @@ import { useNavigate } from "react-router-dom";
 import { SideNavIcons } from "../../data/layout/navbarData";
 import { useSideNav } from "../../context/SideNavProvider";
 import { useUser } from "../../context/UserContext";
-import styles from "../../styles/Sidebar.module.css";
-import { PiRankingLight } from "react-icons/pi";
+import styled from "styled-components";
 
-const SideNavbar = () => {
+interface SidebarWrapperProps {
+  isSideOpen: boolean;
+}
+
+const SidebarWrapper = styled.div<SidebarWrapperProps>`
+  position: fixed;
+  top: 4rem;
+  left: 0;
+  height: 100%;
+  background-color: #f3f4f6; /* bg-gray-100 */
+  border-right: 4px solid #d1d5db;
+  border-left: 4px solid #d1d5db;
+  width: 13%;
+  z-index: 30;
+  transform: ${({ isSideOpen }) =>
+    isSideOpen ? "translateX(120%)" : "translateX(70%)"};
+  transition: transform 0.3s ease-in-out;
+`;
+
+const SideNavbar: React.FC = () => {
   const navigate = useNavigate();
   const { isSideOpen } = useSideNav();
   const { user } = useUser();
 
   return (
-    <div
-      className={`${styles.sidebar} ${
-        isSideOpen ? styles.sidebarOpen : styles.sidebarClosed
-      }`}
-    >
-      {/* <ul className="space-y-4 p-4">
+    <SidebarWrapper isSideOpen={isSideOpen}>
+      <ul className="space-y-4 p-4">
         {SideNavIcons.map(({ Icon, text, path, onClick, loginOnly }, index) =>
           loginOnly === undefined || loginOnly === Boolean(user) ? ( //loginOnly 가 없거나 undefined 면 표시, true면 로그인후에만 표시
             <li
@@ -30,25 +44,8 @@ const SideNavbar = () => {
             </li>
           ) : null
         )}
-      </ul> */}
-
-      <ul className="space-y-4 p-4 absolute">
-        {/* {SideNavIcons.map(({ Icon, text, path, onClick, loginOnly }, index) =>
-          loginOnly === undefined || loginOnly === Boolean(user) ? ( //loginOnly 가 없거나 undefined 면 로그인안해도 가능, true면 로그인후에만 가능
-            <li
-              key={index}
-              className="flex items-center hover:bg-gray-700 p-2 rounded"
-              onClick={() => (path ? navigate(path) : onClick?.())}
-              role="button"
-            >
-              <Icon className="w-7 h-7 hover:scale-110 ml-28" />
-              <div className="ml-4 font-bold">{text}</div>
-            </li>
-          ) : null
-        )} */}
-        <PiRankingLight className="w-7 h-7 hover:scale-110 hover:cursor-pointer hover:bg-gray-400 ml-28" />
       </ul>
-    </div>
+    </SidebarWrapper>
   );
 };
 
