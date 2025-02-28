0ef025c4c204963b95b764e04ab5fc67b1c29fa1
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2025-03-01 01:04:00 +0900

    changed botton navbar bg-color

--- src/components/Layout/Navbar/BottomNavbar.tsx
+++ src/components/Layout/Navbar/BottomNavbar.tsx
@@ -7,12 +7,12 @@ const BottomNavbar = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="bg-mainBlue fixed bottom-0 left-0 w-full h-16 z-50 md:hidden">
-      <div className="flex justify-between text-white  h-full mx-2">
+    <nav className="bg-gray-100 border-t fixed bottom-0 left-0 w-full h-16 z-50 md:hidden">
+      <div className="flex justify-between  h-full mx-2">
         {BottomNavIcons.map(({ Icon, text, path, onClick }, index) => (
           <div
             key={index}
-            className=" flex flex-col items-center justify-center w-full h-full active:scale-110     hover:cursor-pointer"
+            className=" flex flex-col items-center justify-center w-full h-full active:scale-110 hover:cursor-pointer"
             onClick={() => (path ? navigate(path) : onClick?.())}
           >
             <Icon className="w-7 h-7" />
